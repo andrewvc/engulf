2cb0b1a25bc94f640f673896111be0f10ac64a54
Andrew Cholakian <andrew@andrewvc.com>
2011-05-11 00:59:48 -0700

    Remove aleph dependency

--- project.clj
+++ project.clj
@@ -4,5 +4,4 @@
   :aot [parbench.core]
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [clj-http            "0.1.3"]
-                 [aleph "0.1.5-SNAPSHOT"]
                  [org.clojars.automata/rosado.processing "1.1.0"]])

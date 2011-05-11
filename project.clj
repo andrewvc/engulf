da5a2126e6efe59d3f2f0ac3b454691e19877176
Andrew Cholakian <andrew@andrewvc.com>
2011-05-11 01:00:21 -0700

    Disable superfluous aot compilation

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject parbench "1.0.0-SNAPSHOT"
   :description "Parallel HTTP Visualizer"
   :main parbench.core
-  :aot [parbench.core]
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [clj-http            "0.1.3"]
                  [org.clojars.automata/rosado.processing "1.1.0"]])

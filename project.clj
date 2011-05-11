34484a6348a7a2771abb6aed15ac42f6ff95d960
Andrew Cholakian <andrew@andrewvc.com>
2011-05-11 10:24:45 -0700

    Default jvm to -server

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject parbench "1.0.0-SNAPSHOT"
   :description "Parallel HTTP Visualizer"
   :main parbench.core
+  :jvm-opts ["-server"]
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [diamondap/clj-apache-https  "1.0"]

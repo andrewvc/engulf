f6c775143e1e86827f6ddba519fa789de4ee37ac
Andrew Cholakian <andrew@andrewvc.com>
2012-01-29 21:04:44 -0800

    Switch back to -server for jvm

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject parbench "1.3.2"
   :description "Parallel HTTP Benchmarker/Visualizer"
   :main parbench.core
-  :jvm-opts ["-client"]
+  :jvm-opts ["-server"]
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.cli "0.2.1"]
                  [noir-async "0.1.2"]

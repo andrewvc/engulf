7404ab8fb449fa080450a337487a58a2493f62bc
Andrew Cholakian <andrew@andrewvc.com>
2012-01-31 08:40:39 -0800

    Ning now looks more like aleph

--- src/parbench/ning_client.clj
+++ src/parbench/ning_client.clj
@@ -5,10 +5,6 @@
             com.ning.http.client.PerRequestConfig
             com.ning.http.client.AsyncCompletionHandler))
 
-;      AsyncHttpClient c = new AsyncHttpClient(new AsyncHttpClientConfig.Builder().setRequestTimeoutInMs(...).build());
-
-(def client (AsyncHttpClient.))
-
 (defn client-handler [res-ch]
   (proxy [com.ning.http.client.AsyncCompletionHandler] []
           (onCompleted [response]
@@ -20,14 +16,32 @@
             (log/error e "We hit a throwable in Ning")
             (enqueue res-ch e))))
 
-(defn http-get
-  "Convenience method to execute a GET request with the client"
-  [url]
-  (let [handler-res-ch (result-channel)
-        handler (client-handler handler-res-ch)
-        requestConfig (doto (PerRequestConfig.)
-                            (.setRequestTimeoutInMs (int 2000)))]
-    (-> (.prepareGet client url)
-        (.setPerRequestConfig requestConfig)
-        (.execute handler))
-    handler-res-ch))
+(def method-prep-map
+     {:get (memfn prepareGet url)
+      :put (memfn preparePut url)
+      :post (memfn preparePost url)
+      :delete (memfn prepareDelete url)})
+
+(defn http-client [options]
+  "Currently ignores all options"
+  (let [client (AsyncHttpClient.)]
+    (fn this
+      ([request]
+         (this request -1))
+      ([{:keys [method url]} timeout]
+        (let [result (result-channel)
+              handler (client-handler result)
+              requestConfig (doto (PerRequestConfig.)
+                                  (.setRequestTimeoutInMs (int timeout)))]
+          (-> ((get method-prep-map method) client url)
+              (.setPerRequestConfig requestConfig)
+              (.execute handler))
+          result)))))
+                  
+(defn http-request
+  "Aleph style interface"
+  ([request]
+     (http-request request -1))
+  ([request timeout]
+     (let [client (http-client {})]
+       (client request timeout))))

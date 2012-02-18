203769b753c23aaf81474a59014a19d1e0b69949
Andrew Cholakian <andrew@andrewvc.com>
2012-02-18 10:40:05 -0800

    Use latest sonatype snapshot

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :jvm-opts ["-server"]
   :java-source-path "java-src"
   :jar-exclusions [#"\.DS_Store"]
-  :repositories {"Sonatype" "https://oss.sonatype.org/content/repositories/releases/"}
+  :repositories {"Sonatype" "https://oss.sonatype.org/content/repositories/snapshots/"}
   ;; :warn-on-reflection true
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.cli "0.2.1"]
@@ -13,6 +13,6 @@
                  [cheshire "2.0.4"]
                  [log4j/log4j "1.2.16"]
                  [org.slf4j/slf4j-simple "1.6.4"]
-                 [com.ning/async-http-client "1.7.0"]
+                 [com.ning/async-http-client "1.7.1-SNAPSHOT"]
                  [http.async.client "0.4.1"]
                  [org.clojure/tools.logging "0.2.3"]])

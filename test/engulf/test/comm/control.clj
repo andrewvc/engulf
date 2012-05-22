75fb0ac2bfe0ebbb87644a5f31173665d66190ff
Andrew Cholakian <andrew@andrewvc.com>
2012-05-21 20:30:02 -0700

    More tests for net

--- test/engulf/test/comm/control.clj
+++ test/engulf/test/comm/control.clj
@@ -10,16 +10,24 @@
   "Used in testing to reset currently queued messages"
   [& body]
   `(binding [ctrl/node-ch (lc/permanent-channel)]
-    ~@body))
+     ~@body))
+
+(defn clear-nodes
+  []
+  (dosync (ref-set ctrl/nodes {})))
 
 (facts
  "about registering nodes"
  (with-clean-node-ch
+   (clear-nodes)
    (let [ident "a-unique-identifier"
-         n  (ctrl/register-node ident)]
+         n  (ctrl/register-node ident {})]
      (fact
       "the node should have the right uuid"
       (:uuid n) => ident)
+     (fact
+      "the node should have a channel"
+      (:channel n) =not=> nil?)
      (fact
       "the node should have enqueued a creation message"
       (lc/receive ctrl/node-ch
@@ -28,27 +36,28 @@
  (facts
    "for nodes that do currently exist"
    (let [ident "some-unique-id"
-         existing  (ctrl/register-node ident)]
+         existing  (ctrl/register-node ident {})]
      (fact
       "calling create should return nil"
-      (ctrl/register-node ident) => nil))))
+      (ctrl/register-node ident {}) => nil))))
 
 (facts
  "about removing nodes"
+ (clear-nodes)
  (facts
   "for nodes that don't yet exist"
-  (let [ident "a-unique-identifier"]
+  (let [ident "a-unique-identifier"
+        n (ctrl/register-node ident {})]
     (fact
      "the node should no longer be present after removal"
-     (ctrl/register-node ident)
      (ctrl/get-node ident) =not=> nil
-     (ctrl/deregister-node ident)
+     (ctrl/deregister-node n)
      (ctrl/get-node ident) => nil))))
 
 (with-clean-node-ch
   (facts
    "about handling messages"
-   (let [n (ctrl/node "htest-unique-id")
+   (let [n (ctrl/node "htest-unique-id" {})
          msg {:type "htest-type" :body "htest-body"}
          res (ctrl/handle-message n (cmsg/encode-msg msg))
          expected-msg (assoc msg :node n)]

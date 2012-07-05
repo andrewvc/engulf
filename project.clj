097155ef704a4580530e5474a8d5d91159ff81da
Andrew Cholakian <andrew@andrewvc.com>
2012-07-05 10:18:25 -0700

    Lein 2 format

--- project.clj
+++ project.clj
@@ -1,12 +1,4 @@
-(defproject engulf "2.0.0"
-  :description "HTTP Benchmarker/Visualizer"
-  :main engulf.core
-  :jvm-opts ["-server", "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
-  :extra-classpath-dirs ["/usr/lib/jvm/java-6-sun/lib/tools.jar"]
-  :java-source-paths ["java-src"]
-  :jar-exclusions [#"\.DS_Store"]
-  :repositories {"Sonatype" "https://oss.sonatype.org/content/repositories/releases/"}
-  ;; :warn-on-reflection true
+(defproject engulf/engulf "3.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.cli "0.2.1"]
                  [noir-async "1.1.0-beta2"]
@@ -15,7 +7,17 @@
                  [org.slf4j/slf4j-simple "1.6.4"]
                  [com.ning/async-http-client "1.7.4"]
                  [org.clojure/tools.logging "0.2.3"]]
-  :dev-dependencies [[org.clojure/tools.trace "0.7.3"]
-                     [lein-midje "1.0.9"]
-                     [lein-swank "1.4.4"]
-                     [midje "1.4.0"]])
+  :profiles {:dev
+             {:resource-paths
+              ["/usr/lib/jvm/java-6-sun/lib/tools.jar"],
+              :dependencies
+              [[org.clojure/tools.trace "0.7.3"] [midje "1.4.0"]]}}
+  :repositories {"Sonatype"
+                 "https://oss.sonatype.org/content/repositories/releases/"}
+  :java-source-paths ["java-src"]
+  :main engulf.core
+  :min-lein-version "2.0.0"
+  :jvm-opts ["-server"
+             "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
+  :plugins [[lein-midje "1.0.9"] [lein-swank "1.4.4"]]
+  :description "HTTP Benchmarker/Visualizer")

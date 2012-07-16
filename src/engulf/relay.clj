1c5296d37a80b8c130777f343f1e8fb28625aa0b
Andrew Cholakian <andrew@andrewvc.com>
2012-07-15 20:47:32 -0700

    Rough relay

---
+++ src/engulf/relay.clj
@@ -0,0 +1,24 @@
+(ns engulf.relay
+  (:require
+   [clojure.tools.logging :as log]
+   [engulf.formula :as formula]
+   [lamina.core :as lc]))
+
+(def start-cb (agent nil))
+
+(defn start
+  []
+  (send start-cb
+        (fn [v]
+          (when v
+            (lc/cancel-callback v))
+          (lc/receive-all
+           ctrl/broadcast
+           (fn [m] (println "GOT M!" m))))))
+
+(defn stop
+  []
+  (send start-cb
+        (fn [v]
+          (when v
+            (lc/cancel-callback v)))))

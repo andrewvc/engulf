f4e8b766ee21f4f9d6db9539ef5aca65a58ea50a
Andrew Cholakian <andrew@andrewvc.com>
2012-07-26 21:42:20 -0700

    Latest noir async

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject engulf/engulf "3.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.cli "0.2.1"]
-                 [noir-async "1.1.0-beta6"]
+                 [noir-async "1.1.0-beta7"]
                  [cheshire "4.0.0"]
                  [log4j/log4j "1.2.16"]
                  [org.slf4j/slf4j-simple "1.6.4"]

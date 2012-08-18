23da39a9b185c1f31f69e280cf64f4319c971046
Andrew Cholakian <andrew@andrewvc.com>
2012-08-18 10:17:54 -0700

    Bugfixes for AMI bootstrap

--- misc/ami_bootstrap.sh
+++ misc/ami_bootstrap.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-curl -L http://engulf-project.org/latest.jar > ~/engulf.jar
-curl http://169.254.169.254/latest/user-data | head -n1 > ~/engulf-opts
-ENGULF_OPTS=`cat ~/engulf-opts`
+INST_DIR=/home/ubuntu
+curl -L http://engulf-project.org/latest.jar > $INST_DIR/engulf.jar
+curl http://169.254.169.254/latest/user-data | head -n1 > $INST_DIR/engulf-opts
+ENGULF_OPTS=`cat $INST_DIR/engulf-opts`
 MEMTOT=`free -m | grep '^Mem: ' | awk '{print $2}'`
 MEMUNIT=m
 MEMMIN=`expr $MEMTOT - 400`$MEMUNIT
 MEMMAX=`expr $MEMTOT - 200`$MEMUNIT
-CMD="java -Xms$MEMMIN -Xmx$MEMMAX -jar $HOME/engulf.jar --http-port 8080 $ENGULF_OPTS"
+CMD="java -Xms$MEMMIN -Xmx$MEMMAX -jar $INST_DIR/engulf.jar --http-port 8080 $ENGULF_OPTS"
 echo Will run: $CMD
 $CMD

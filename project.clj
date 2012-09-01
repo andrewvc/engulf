17af3f305fe8c0c525a071267f5fe326771fdb29
Andrew Cholakian <andrew@andrewvc.com>
2012-09-01 10:23:28 -0700

    Removed unused ning/AHC dependency

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
                  [noir-async "1.1.0-beta9"]
                  [cheshire "4.0.0"]
                  [org.slf4j/slf4j-simple "1.6.6"]
-                 [com.ning/async-http-client "1.7.4"]
                  [org.xerial/sqlite-jdbc "3.6.16"]
                  [korma "0.3.0-beta9"]
                  [ragtime "0.2.1"]
